6e655915d5c5f42117e933d6580b3e3c9b6b5f88
dbruner23 <dbruner23@gmail.com>
2022-09-01 23:31:44 +1200

    hoping for the best

--- Controllers/SearchController.js
+++ Controllers/SearchController.js
@@ -4,7 +4,7 @@ const getRego = async function (req, res) {
     const { rego } = req.params;
 
     try {
-        const response = await axios.get(`http://api.carsxe.com/platedecoder?key=mxt1nrqj6_cyicji008_s31yhcqle&plate=${rego}&state=NZ&format=json`)
+        const response = await axios.get(`http://api.carsxe.com/platedecoder?key=${process.env.CARS_API_KEY}&plate=${rego}&state=NZ&format=json`)
         res.status(200).json(response.data);
     } catch (error) {
         res.status(404).json("Sorry you've got an error." + error)
