3cb8684283cce819188735b43940e3fe1b59c96a
dbruner23 <dbruner23@gmail.com>
2022-09-02 10:27:16 +1200

    retrying env

--- Controllers/SearchController.js
+++ Controllers/SearchController.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 
-const getRego = async function (req, res) {
+const getRego = async (req, res) => {
     const { rego } = req.params;
 
     try {
-        const response = await axios.get(`http://api.carsxe.com/platedecoder?key=8ti0mvx6x_m2ts2jzmw_tf6gw7d3a&plate=${rego}&state=NZ&format=json`)
+        const response = await axios.get(`http://api.carsxe.com/platedecoder?key=${process.env.CARS_API_KEY}&plate=${rego}&state=NZ&format=json`)
         res.status(200).json(response.data);
     } catch (error) {
         res.status(404).json("Sorry you've got an error." + error)
--- Routes/__test__/index.test.js
+++ Routes/__test__/index.test.js
@@ -1,15 +1,15 @@
 const app = require('../../app.js');
 const request = require('supertest');
 
-describe('regoSearch', function () {
-    it('returns status code 200 and car data if valid rego number is passed', async function () {
+describe('regoSearch', () => {
+    it('returns status code 200 and car data if valid rego number is passed', async () => {
         const res = await request(app).get('/regosearch/khz762');
 
         expect(res.statusCode).toEqual(200);
         expect(res.body.CarMake).toBe('Subaru');
     })
 
-    it('returns bad request if valid rego number is missing', async function () {
+    it('returns bad request if valid rego number is missing', async () => {
         const res = await request(app).get('/regosearch/');
 
         expect(res.statusCode).toEqual(404);
