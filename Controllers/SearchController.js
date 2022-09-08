601683ec25a3e9f720e1f1592140b26e565e8713
dbruner23 <dbruner23@gmail.com>
2022-09-08 13:03:55 +1200

    successfully integrate with frontend, but will not deploy - using mock response

--- Controllers/SearchController.js
+++ Controllers/SearchController.js
@@ -25,14 +25,14 @@ const mockResponse = {
 
 const getRego = async (req, res) => {
     const { rego } = req.params;
-    // res.status(200).json(mockResponse)
+    res.status(200).json(mockResponse)
 
-    try {
-        const response = await axios.get(`http://api.carsxe.com/platedecoder?key=8ti0mvx6x_m2ts2jzmw_tf6gw7d3a&plate=${rego}&state=NZ&format=json`)
-        res.status(200).json(response.data);
-    } catch (error) {
-        res.status(404).json("Sorry you've got an error." + error)
-    }
+    // try {
+    //     const response = await axios.get(`http://api.carsxe.com/platedecoder?key=8ti0mvx6x_m2ts2jzmw_tf6gw7d3a&plate=${rego}&state=NZ&format=json`)
+    //     res.status(200).json(response.data);
+    // } catch (error) {
+    //     res.status(404).json("Sorry you've got an error." + error)
+    // }
 }
 
 module.exports = {
